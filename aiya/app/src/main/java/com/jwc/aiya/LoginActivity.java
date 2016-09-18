aedfa73261fece3125661d76f1ebd5f2820a3b2c
Jingweicheng <m18856756536@163.com>
2016-09-18 10:36:10 +0800

    登陆UI的编写

---
+++ aiya/app/src/main/java/com/jwc/aiya/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.jwc.aiya;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jwc on 2016/9/14.
+ */
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
